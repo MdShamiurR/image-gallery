1f2445ae16ab88a8f93b85c2664744b35c3b616d
MdShamiurR <shamiur.rahman1997@gmail.com>
2023-11-06 04:04:57 +0600

    [DONE]HandleDelete Dragging&Dropping BugFixing

--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -1,81 +1,135 @@
+import update from "immutability-helper";
 import { useEffect, useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import addImg from "../../../../images/addImg.png";
 import "./Gallery.css";
 import GalleryCard from "./GalleryCard";
 
 const Gallery = () => {
-  const [Galleries, setGalleries] = useState([]);
   const [state, setState] = useState({
     count: 0,
     selected: [],
     photos: [], // Initialize photos as an empty array
-    // addImages: [],
   });
 
  useEffect(() => {
    // Fetch the gallery data from gallery.json
    fetch("gallery.json")
      .then((res) => res.json())
-     .then((data) => {
-       setGalleries(data);
-       // Now that Galleries is set, update photos
+     .then((data) =>
        setState({
          ...state,
          photos: data,
-       });
-     });
+       })
+     );
  }, []);
 
 
   const { photos, selected ,count} = state;
-  console.log("all I", photos);
 
-  const handleIPhotoUploader = (up) => {
+
+const handleIPhotoUploader = (event) => {
+  const selectedFile = event.target.files[0];
+
+  if (selectedFile) {
     const newImage = {
       gallery_id: photos.length + 1,
-      img: URL.createObjectURL(up.target.files[0]),
+      img: URL.createObjectURL(selectedFile),
     };
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        photos: [...prevState.photos, newImage],
+      };
+    });
+  }
+};
+
+  const handelDelete = () => {
+    const selectedIds = new Set(selected); // create a set of selected gallery IDs
+
+    const remainingPhotos = photos.filter(
+      (photo) => !selectedIds.has(photo.gallery_id)
+    );
+
     setState({
       ...state,
-      photos: [...photos, newImage],
+      count: 0, // reset count to 0
+      selected: [], // clear the selected items
+      photos: remainingPhotos, // update the remaining photos
     });
   };
 
+  const draggingGrid = (index, hoverIndex) => {
+    setState(
+      update(state, {
+        photos: {
+          $splice: [
+            [index, 1],
+            [hoverIndex, 0, photos[index]],
+          ],
+        },
+      })
+    );
+  };
+
   return (
-    <div className="mt-14">
-      
-      <div className="grid grid-cols-5 grid-rows-3 gap-4">
-        {photos.map((gallery, index) => (
-          <GalleryCard
-            key={gallery.gallery_id}
-            gallery={gallery}
-            index={index}
-          />
-        ))}
-        {/* Add an input field for uploading new images */}
-        <div
-          style={{
-            border: "2px dotted gray",
-            borderRadius: "5px",
-            position: "relative",
-          }}
-        >
-          <img src={addImg} alt="addImage" />
-          <input
-            style={{
-              position: "absolute",
-              left: "0",
-              top: "0",
-              width: "100%",
-              height: "100%",
-              opacity: 0, // Hide the file input
-            }}
-            type="file"
-            accept="image/*" // Accept all image types
-            onChange={handleIPhotoUploader}
-          />
-        </div>
+    <div className="mt-24 max-w-5xl photo-gallery p-5 bg-zinc-100 shadow shadow-black m-auto ">
+      <div className="flex justify-between items-center border-b-2">
+        {count > 0 ? (
+          <>
+            <h3 className="font-bold p-4 ">
+              <input type="checkbox" defaultChecked={true} /> {count} File
+              Selected
+            </h3>
+            <p className="font-bold p-4 text-red-600 " onClick={handelDelete}>
+              Delete files
+            </p>
+          </>
+        ) : (
+          <>
+            <h3 className="font-bold p-4 ">Gallery</h3>
+          </>
+        )}
       </div>
+
+      <DndProvider backend={HTML5Backend}>
+        <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-5 grid-rows-3  md:grid-rows-2 gap-4 p-5 photoGrid">
+          {photos.map((gallery, index) => (
+            <GalleryCard
+              key={index}
+              gallery={gallery}
+              index={index}
+              state={state}
+              // handelSet={setState}
+              setState={setState}
+              draggingGrid={draggingGrid}
+            />
+          ))}
+          {/* for uploading new images */}
+          <div
+            style={{
+              border: "2px dotted gray",
+              borderRadius: "5px",
+              position: "relative",
+            }}
+          >
+            <img src={addImg} alt="addImage" />
+            <input
+              className="w-full h-full photoUploader"
+              style={{
+                opacity: 0, // Hide the file input
+                cursor: "pointer",
+              }}
+              type="file"
+              accept="image/*" // Accept all image types
+              onChange={handleIPhotoUploader}
+            />
+          </div>
+        </div>
+      </DndProvider>
     </div>
   );
 };
--- src/Pages/Home/Gallery/GalleryCard.jsx
+++ src/Pages/Home/Gallery/GalleryCard.jsx
@@ -1,30 +1,144 @@
-// import addImg from "../../../../images/addImg.png";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useDrag, useDrop } from "react-dnd";
 
 
-const GalleryCard = ({gallery,index}) => {
-  const { img,gallery_id } = gallery;
-
+const GalleryCard = (props) => {
+  const { gallery, state, setState, index, draggingGrid } = props;
+  const [checked, setChecked] = useState(false);
+  const ref = useRef(null);
 
   const isFirstService = index === 0;
 
   const dynamicStyle = isFirstService ? { gridRow: "1 / 3", gridColumn: "1 / 3" } : {};
 
+
+const [{ handlePhotoId }, drop] = useDrop({
+  accept: "image",
+  collect(screen) {
+    return {
+      handlePhotoId: screen.getHandlerId(),
+    };
+  },
+  hover(product, screen) {
+    const dragIndex = product.index;
+    if (dragIndex === index) {
+      return;
+    }
+
+    const hoverBoundingClientRect = ref.current?.getBoundingClientRect();
+    const hoverMiddleY = (hoverBoundingClientRect.bottom - hoverBoundingClientRect.top) / 2;
+    const clientOffset = screen.getClientOffset();
+    const hoverClientY = clientOffset.y - hoverBoundingClientRect.full;
+
+    if (
+      (dragIndex < index && hoverClientY < hoverMiddleY) ||
+      (dragIndex > index && hoverClientY > hoverMiddleY)
+    ) {
+      return;
+    }
+
+    draggingGrid(dragIndex, index);
+    product.index = index;
+  },
+});
+
+  
+
+const [{ isDragging }, drag] = useDrag({
+  type: "image",
+  options: {
+    dropEffect: "copy",
+  },
+  item: () => {
+    return { index };
+  },
+  collect: (screen) => ({
+    isDragging: screen.isDragging(),
+  }),
+});
+const opacity = isDragging ? 0 : 1;
+drag(drop(ref));
+
+  const handleCheckboxChange = useCallback(
+    (photoId) => {
+      // console.log("this is photoId",photoId)
+      if (!checked) {
+        setState({
+          ...state,
+          count: state.count + 1,
+          selected: [...state.selected, photoId],
+        });
+      } else {
+        setState({
+          ...state,
+          count: state.count - 1,
+          selected: state.selected.filter((item) => item !== photoId),
+        });
+      }
+    },
+    [setState, state, checked]
+  );
+
+ useEffect(() => {
+   if (state.count === 0) {
+     setChecked(false);
+   }
+ }, [state]);
   return (
     <div
+      ref={ref}
+      data-handler-id={handlePhotoId}
       draggable
-      className="card bg-base-100"
+      className="card bg-base-100 "
       style={{
         ...dynamicStyle,
+
+        opacity,
         border: "1px solid gray",
         cursor: "move",
         borderRadius: "10px",
+        
       }}
     >
-      <div>
-        <img src={img} alt="Shoes" />
+      <img
+        style={{ objectFit: "cover" }}
+        className="w-full h-full border-1 rounded-lg"
+        src={gallery.img}
+        alt="Shoes"
+      />
+      {checked && state.count > 0 && (
+        <div
+          style={{ position: "absolute", top: "0", left: "0", padding: "1rem" }}
+        >
+          <input
+            defaultChecked={
+              !(checked && state.count && state.selected.length > 0)
+                ? false
+                : true
+            }
+            type="checkbox"
+          />
+        </div>
+      )}
+
+      <div
+        style={{ position: "absolute", cursor: "pointer" }}
+        className="hide w-full h-full bg-[#00000067] rounded p-4"
+      >
+        <input
+          
+          onClick={() => handleCheckboxChange(gallery.gallery_id)}
+          onChange={() => {
+            setChecked(!checked);
+          }}
+          type="checkbox"
+          checked={checked && state.count > 0 && state.selected.length > 0}
+        />
       </div>
     </div>
   );
 };
 
 export default GalleryCard;
+
+
